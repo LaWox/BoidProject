5789fb396d83a933671323a8342165216776268a
Platon Woxler <woxler.platon@gmail.com>
2024-12-18 18:15:23 +0100

    feat(flockingBehaviour): Working separation, cohesion and alignment  (#1)
    * debugging separation
    
    * one last try before bed
    
    * seperation working more as inteneded
    
    * better debugging + tweaks to direction vectors
    
    * WIP

--- Assets/Scripts/BoidBehaviour/Boid.cs
+++ Assets/Scripts/BoidBehaviour/Boid.cs
@@ -1,25 +1,108 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using TreeEditor;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering;
 using Object = UnityEngine.Object;
 
 public class Boid : MonoBehaviour
 {
-    private Vector3 _velocity = Vector3.zero;
-    private Flock _flock = null;
+    private Vector3 _velocity;
+    private Vector3 _prevPos;
+
+    private Flock _flock;
+    private Vector3 _separation = Vector3.zero;
+    private Vector3 _cohesion = Vector3.zero;
+    private Vector3 _alignment = Vector3.zero;
+    private List<Boid> _neighbours;
+    private float _scaling;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        _flock = transform.parent?.GetComponent<Flock>();
+        _velocity = Vector3.forward;
+        _flock = transform.parent.GetComponent<Flock>();
+        _prevPos = transform.position;
+        _neighbours = new List<Boid>();
+        _scaling = _flock is not null ?  _flock.alignmentFactor + _flock.cohesionFactor + _flock.alignmentFactor + _flock.boidInertia : 1f;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (_flock is null){return;}
+        _velocity = (transform.position - _prevPos) / Time.deltaTime;
+        _prevPos = transform.position;
         
+        if (Time.frameCount % 10 == 0) // update every 10:th frame
+        {
+            _neighbours = GetNeighbours();
+        }
+        
+        
+        _separation = GetSeparationDirection() * _flock.seperationFactor;
+        _cohesion = GetCohesionDirection() * _flock.cohesionFactor;
+        _alignment = GetAlignmentDirection() * _flock.alignmentFactor;
+        _velocity = _velocity.normalized * _flock.boidInertia;
+
+        Debug.DrawLine(transform.position, transform.position + _separation, Color.red);
+        Debug.DrawLine(transform.position, transform.position + _cohesion, Color.green);
+        Debug.DrawLine(transform.position, transform.position +_alignment, Color.blue);
+        Debug.DrawLine(transform.position, transform.position + _velocity, Color.yellow);
+        Debug.DrawLine(transform.position, transform.position + Vector3.forward * _flock.influenceRadius, Color.magenta);
+        
+
+        var direction = (_separation + _cohesion + _alignment + _velocity) / _scaling;
+        
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + direction, _flock.speed * Time.deltaTime); 
     }
 
     public Vector3 GetVelocity()
     {
         return _velocity;
+    }
+    
+    private List<Boid> GetNeighbours()
+    {
+        var neighbours = new List<Boid>();
+        foreach (var boid in _flock.boids)
+        {
+            if ((transform.position - boid.transform.position).magnitude < _flock.influenceRadius && boid.GetInstanceID() != transform.GetInstanceID())
+            {
+                neighbours.Add(boid);
+            }
+        }
+        return neighbours;
+    }
+    
+    private Vector3 GetSeparationDirection()
+    {
+        var totalSeparation = Vector3.zero;
+        foreach (var boid in _neighbours)
+        {
+            var separationVector = transform.position - boid.transform.position;
+            var magnitude = separationVector.magnitude;
+            if (magnitude > 0)
+            {
+                totalSeparation += separationVector.normalized / magnitude;
+            }
+        }
+        return totalSeparation;
+    }
+    
+    private Vector3 GetAlignmentDirection()
+    {
+        var alignmentDirection = _neighbours.Aggregate(Vector3.zero, (current, boid) => current + boid.GetVelocity());
+        return alignmentDirection.normalized;
+    }
+
+    private Vector3 GetCohesionDirection()
+    {
+        var avgPos = _neighbours
+            .Select(boid => boid.transform.position)
+            .Aggregate(Vector3.zero, (current, pos) => current + pos) / (float) _neighbours.Count();
+        return (avgPos - transform.position).normalized;
     }
 }
--- Assets/Scripts/BoidBehaviour/Flock.cs
+++ Assets/Scripts/BoidBehaviour/Flock.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Linq;
 using UnityEngine;
 using Random = UnityEngine.Random;
@@ -6,40 +5,39 @@ using Random = UnityEngine.Random;
 public class Flock : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    private GameObject[] _boids;
+    public Boid[] boids;
     public GameObject boidPrefab;
     
+    [Range(1, 10)]
     public int totalBoids;
-    public float seperation;
-    public float speed;
+    [Range(0, 1)]
+    public float seperationFactor;
+    [Range(0, 10)]
+    public float alignmentFactor;
+    [Range(0, 10)]
+    public float cohesionFactor;
+    [Range(0, 10)]
+    public float influenceRadius;   
+    [Range(0, 1)]
+    public float speed;   
+    [Range(0, 1)]
+    public float boidInertia;
     
     void Start()
     {
-        _boids = new GameObject[totalBoids];
+        boids = new Boid[totalBoids];
+
         for (var i = 0; i < totalBoids; i++)
         {
-            _boids[i] = Instantiate(boidPrefab, new Vector3(
-                Random.Range(0f, 1f), // Random x
-                Random.Range(0f, 1f), // Random y
-                Random.Range(0f, 1f)  // Random z
-            ), Quaternion.identity);
+            var tempPrefab = Instantiate(boidPrefab, transform);
+            tempPrefab.name = "Boid" + i;
+            tempPrefab.transform.position = new Vector3(
+                Random.Range(0f, 1f),
+                Random.Range(0f, 1f),
+                Random.Range(0f, 1f)
+            );
+            
+            boids[i] = tempPrefab.GetComponent<Boid>();
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    Vector3 GetAvgVelocity()
-    {
-        if (_boids == null || _boids.Length == 0)
-        {
-            return Vector3.zero;
-        }
-        var directions = _boids.Select(boid => boid.GetComponent<Boid>().GetVelocity());
-        var avgDirection = directions.Aggregate(Vector3.zero, (current, direction) => current + direction);
-        return avgDirection / (float) totalBoids;
     }
 }
